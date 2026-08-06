35b4e9a90f822af516bef26e6f5dbc81dfda7ae1
annandajacobs <annandajacobs@gmail.com>
2026-08-06 12:02:16 -0300

    feat(db): adiciona modelos e migrações de preferências, objetivos e histórico

--- api/database/base.py
+++ api/database/base.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import DeclarativeBase
 
+
 class Base(DeclarativeBase):
     """
     Classe base para os modelos do SQLAlchemy. Todos os modelos devem herdar desta classe para garantir a consistência e a integração com o ORM.
     """
-    pass
---
+++ api/models/__init__.py
@@ -0,0 +1,6 @@
+from api.models.reading_history import ReadingHistory
+from api.models.user import User
+from api.models.user_goal import UserGoal
+from api.models.user_preference import UserPreference
+ 
+__all__ = ["ReadingHistory", "User", "UserGoal", "UserPreference"]
